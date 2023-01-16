a49f81b74399a2d4d54d7eacedca0d2a1c23c5fc
na2na-p <na2na@na2na.dev>
2023-01-16 18:16:54 +0900

    Update users migration

--- packages/backend/database/migrations/2023_01_16_013845_create_users_table.php
+++ packages/backend/database/migrations/2023_01_16_013845_create_users_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->char('id', 36)->primary()->comment('バックエンドでUUID生成');
             $table->timestamps();
+            $table->string('name', 64)->comment('日本語英語問わず64、登録なしはできない');
+            $table->string('password', 255)->comment('ハッシュ化前は8文字以上 32文字以下');
+            $table->string('email', 255)->unique()->comment('ユーザメールアドレス');
         });
     }
 
