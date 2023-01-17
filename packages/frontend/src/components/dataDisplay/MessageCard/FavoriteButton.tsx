d4b7a268c24736ee8c92786d25354be1cb26e4de
na2na-p <na2na@na2na.dev>
2023-01-17 14:09:17 +0900

    Add FavoriteButton

---
+++ packages/frontend/src/components/dataDisplay/MessageCard/FavoriteButton.tsx
@@ -0,0 +1,23 @@
+import StarIcon from '@/components/icons/StarIcon';
+import Button from '@/components/input/Button';
+
+const FavoriteButton = ({
+	count,
+	isFavorited
+}: {
+	count: number,
+	isFavorited: boolean
+}) => {
+	return (
+		<Button
+			variant="text"
+			startIcon={<StarIcon
+				variant={isFavorited ? 'filled' : 'outlined'}
+			/>}
+			label={`${count}`}
+			color='primary'
+		/>
+	);
+};
+
+export default FavoriteButton;
