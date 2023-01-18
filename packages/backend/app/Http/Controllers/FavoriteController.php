93dee4a9d4ca25360fa2b4cde8ffdc1419d7bed4
na2na-p <na2na@na2na.dev>
2023-01-19 01:56:07 +0900

    Fix Lint

--- packages/backend/app/Http/Controllers/FavoriteController.php
+++ packages/backend/app/Http/Controllers/FavoriteController.php
@@ -6,9 +6,7 @@ use App\Http\Requests\addFavoriteRequest;
 use App\Models\Favorite;
 use App\Models\Message;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
 use Ramsey\Uuid\Uuid;
 
 class FavoriteController extends Controller
@@ -16,12 +14,15 @@ class FavoriteController extends Controller
     /**
      * お気に入り登録用エンドポイント
      *
-     * @param addFavoriteRequest $request
-     * @param string $messageId
+     * @param  addFavoriteRequest  $request
+     * @param  string  $messageId
      * @return Response
      */
     public function addFavorite(addFavoriteRequest $request, string $messageId): Response
     {
+        $user = $request->user();
+        assert($user !== null);
+
         $validated = $request->validated();
 
         $message = Message::find($messageId);
@@ -36,7 +37,7 @@ class FavoriteController extends Controller
 
             $favorite = new Favorite();
             $favorite->id = $uuid->toString();
-            $favorite->user_id = $request->user()->id;
+            $favorite->user_id = $user->id;
             $favorite->message_id = $messageId;
 
             try {
