5bfe78553a2dfd57bc75611a28d9c9b35f3c4f8a
na2na-p <na2na@na2na.dev>
2023-01-18 19:58:38 +0900

    Add dependencies

--- packages/backend/config/app.php
+++ packages/backend/config/app.php
@@ -186,7 +186,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Vyuldashev\LaravelOpenApi\OpenApiServiceProvider::class,
         /*
          * Application Service Providers...
          */
