79d6f0abc7146ffdcd4a2ef3ebe1c47ca6c6e9da
na2na-p <na2na@na2na.dev>
2023-01-19 00:15:47 +0900

    Update Header

--- packages/frontend/src/layout/Header/index.tsx
+++ packages/frontend/src/layout/Header/index.tsx
@@ -6,22 +6,25 @@ import Box from '@/components/layout/Box';
 import Stack from '@/components/layout/Stack';
 import Toolbar from '@/components/navigation/Toolbar';
 import AppBar from '@/components/surfaces/AppBar';
+import useAuth from '@/hooks/useAuth';
 
 const APP_NAME = 'sns-app';
 
 const Header = () => {
+	const { isAuthenticated } = useAuth();
 	return (
-		<Box sx={{ flexGrow: 1 }}>
+		<Box>
 			<AppBar position="static">
 				<Toolbar>
 					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 						{APP_NAME}
 					</Typography>
-					{/* TODO: ログアウト時はボタン類全て非表示 */}
-					<Stack direction='row'>
-						<Button color='inherit' label='情報編集' startIcon={<EditIcon />} />
-						<Button color='inherit' label='ログアウト' startIcon={<LogoutIcon />} />
-					</Stack>
+					{isAuthenticated && (
+						<Stack direction='row'>
+							<Button color='inherit' label='情報編集' startIcon={<EditIcon />} />
+							<Button color='inherit' label='ログアウト' startIcon={<LogoutIcon />} />
+						</Stack>
+					)}
 				</Toolbar>
 			</AppBar>
 		</Box>
