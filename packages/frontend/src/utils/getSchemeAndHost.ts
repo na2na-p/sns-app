401bcf9d7292956180b0e81db275bd34efebeaf7
na2na-p <na2na@na2na.dev>
2023-01-19 23:45:17 +0900

    fix prefix

--- packages/frontend/src/utils/getSchemeAndHost.ts
+++ packages/frontend/src/utils/getSchemeAndHost.ts
@@ -1,6 +1,6 @@
 const getSchemeAndHost = (): string => {
 	const { location } = window;
-	return `${location.protocol}//${process.env.NODE_ENV === 'development' ? '' : 'api.'}${location.hostname}:${process.env.NODE_ENV === 'development' ? 80 : 443}`;
+	return `${location.protocol}//${process.env.NODE_ENV === 'development' ? '' : 'api-'}${location.hostname}:${process.env.NODE_ENV === 'development' ? 80 : 443}`;
 };
 
 export default getSchemeAndHost;
