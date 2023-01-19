94deb5cece7018c6f1f4d20f9e1531f886923be8
na2na-p <na2na@na2na.dev>
2023-01-19 14:20:57 +0900

    ビルドテストつらいのでgeneratedも入れる

---
+++ packages/frontend/src/generated/index.ts
@@ -0,0 +1,6 @@
+export * from './badRequestResponse';
+export * from './badRequestResponseErrors';
+export * from './forbiddenResponse';
+export * from './postApiV1LoginBody';
+export * from './postApiV1UsersBody';
+export * from './userResponse';
