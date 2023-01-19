75e14d139a3b115fe3d6ac0792b4c40a8bb3e2ad
na2na-p <na2na@na2na.dev>
2023-01-19 23:22:46 +0900

    Fix ports

--- packages/frontend/src/utils/getSchemeAndHost.ts
+++ packages/frontend/src/utils/getSchemeAndHost.ts
@@ -1,6 +1,6 @@
 const getSchemeAndHost = (): string => {
 	const { location } = window;
-	return `${location.protocol}//${process.env.NODE_ENV === 'development' ? '' : 'api.'}${location.hostname}:${location.protocol === 'https' ? 443 : 80}`;
+	return `${location.protocol}//${process.env.NODE_ENV === 'development' ? '' : 'api.'}${location.hostname}:${process.env.NODE_ENV === 'development' ? 80 : 443}`;
 };
 
 export default getSchemeAndHost;
