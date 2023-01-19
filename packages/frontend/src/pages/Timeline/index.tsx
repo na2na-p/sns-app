b49e4098d743af0af2ccdec4ca237e97bb0ab4fa
na2na-p <na2na@na2na.dev>
2023-01-19 19:31:36 +0900

    Add buttons

--- packages/frontend/src/pages/Timeline/index.tsx
+++ packages/frontend/src/pages/Timeline/index.tsx
@@ -1,28 +1,48 @@
+import { SpeedDialAction, SpeedDialIcon } from '@mui/material';
+
 import ScrollContainer from '@/components/dataDisplay/ScrollContainer';
 import Typography from '@/components/dataDisplay/Typography';
+import EditIcon from '@/components/icons/EditIcon';
 import Stack from '@/components/layout/Stack';
+import SpeedDial from '@/components/navigation/SpeedDial';
 import GrayContainer from '@/layout/GrayContainer';
 
 import InfiniteMessages from './InfiniteMessages';
 
 const Timeline = () => {
 	return (
-		<Stack
-			spacing={0}
-			height="100%"
-			sx={{
-				minHeight: 0
-			}}
-		>
-			<Typography variant="h4">タイムライン</Typography>
-			<GrayContainer width="100%" height="100%">
-				<ScrollContainer>
-					<Stack spacing={2}>
-						<InfiniteMessages />
-					</Stack>
-				</ScrollContainer>
-			</GrayContainer>
-		</Stack>
+		<>
+			<SpeedDial
+				ariaLabel="Menu"
+				icon={<SpeedDialIcon />}
+				sx={{
+					position: 'fixed',
+					right: 50,
+					bottom: 50
+				}}>
+				<SpeedDialAction
+					key={'openModal'}
+					icon={<EditIcon />}
+					tooltipTitle="投稿する"
+				/>
+			</SpeedDial>
+			<Stack
+				spacing={0}
+				height="100%"
+				sx={{
+					minHeight: 0
+				}}
+			>
+				<Typography variant="h4">タイムライン</Typography>
+				<GrayContainer width="100%" height="100%">
+					<ScrollContainer>
+						<Stack spacing={2}>
+							<InfiniteMessages />
+						</Stack>
+					</ScrollContainer>
+				</GrayContainer>
+			</Stack>
+		</>
 	);
 };
 
