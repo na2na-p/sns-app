e95c88eefe19dd3f93663267d30e9507c19ece77
na2na-p <na2na@na2na.dev>
2023-01-19 11:44:52 +0900

    Add sign-up button

--- packages/frontend/src/pages/Login/index.tsx
+++ packages/frontend/src/pages/Login/index.tsx
@@ -1,54 +1,72 @@
+import { useNavigate } from 'react-router-dom';
+
 import Card from '@/components/dataDisplay/Card';
 import Button from '@/components/input/Button';
 import TextInput from '@/components/input/TextInput';
+import Box from '@/components/layout/Box';
 import Stack from '@/components/layout/Stack';
+import routes from '@/routes';
 
 import { useHooks } from './hooks';
 import SHADOW from './SHADOW';
 
 const Login = () => {
 	const { register, handleSubmit, onSubmit, errors } = useHooks();
+	const navigate = useNavigate();
+	const signUpNavigate = () => {
+		navigate(routes.signUp.path());
+	};
 	return (
-		<Card
-			sx={{
-				width: 500,
-				height: 250,
-				radius: 4,
-				border: `solid 1px #E4E5E6`,
-				boxShadow: SHADOW,
-				padding: '30px',
-				position: 'absolute',
-				top: '50%',
-				left: '50%',
-				transform: 'translate(-50%,-50%)'
-			}}
-		>
+		<Box sx={{
+			position: 'absolute',
+			top: '50%',
+			left: '50%',
+			transform: 'translate(-50%,-50%)'
+		}}>
 			<Stack>
-				<TextInput
-					required
-					type="email"
-					placeholder="Email"
-					{...register('email')}
-					error={'email' in errors}
-					helperText={errors.email?.message}
-				/>
-				<TextInput
-					required
-					type="password"
-					placeholder="Password"
-					{...register('password')}
-					error={'password' in errors}
-					helperText={errors.password?.message}
-				/>
+				<Card
+					sx={{
+						width: 500,
+						height: 325,
+						radius: 4,
+						border: `solid 1px #E4E5E6`,
+						boxShadow: SHADOW,
+						padding: '30px'
+					}}
+				>
+					<Stack height="100%" spacing={5}>
+						<TextInput
+							required
+							type="email"
+							placeholder="Email"
+							{...register('email')}
+							error={'email' in errors}
+							helperText={errors.email?.message}
+						/>
+						<TextInput
+							required
+							type="password"
+							placeholder="Password"
+							{...register('password')}
+							error={'password' in errors}
+							helperText={errors.password?.message}
+						/>
+						<Button
+							color="primary"
+							variant="contained"
+							label="ログイン"
+							size="large"
+							onClick={handleSubmit(onSubmit)}
+						/>
+					</Stack>
+				</Card>
 				<Button
-					color="primary"
-					variant="contained"
-					label="ログイン"
-					size="large"
-					onClick={handleSubmit(onSubmit)}
+					label='新規登録はこちら'
+					variant="outlined"
+					onClick={signUpNavigate}
 				/>
 			</Stack>
-		</Card>
+		</Box>
 	);
 };
 
