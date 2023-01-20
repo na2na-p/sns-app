08780b97b62fe5415024efc57b525d23bd083878
na2na-p <na2na@na2na.dev>
2023-01-20 15:58:15 +0900

    Add auth controllers test - Login - Logout

---
+++ packages/backend/tests/Feature/Http/Controllers/LoginControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * ログインが正しく動作するか
+     *
+     * @return void
+     */
+    public function testLogin(): void
+    {
+        $this->assertGuest();
+        $password = 'password';
+        User::factory()->create();
+        $user = User::first();
+        $response = $this->post('/api/v1/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+        $response->assertStatus(200);
+        $this->assertAuthenticated();
+    }
+
+    /**
+     * IDパスワードが間違っている場合に正しく弾けるか
+     *
+     * @dataProvider signupUsersArgsValidationDataProvider
+     *
+     * @param  array  $data
+     * @return void
+     */
+    public function testLoginWithInvalidArgs(array $data): void
+    {
+        $this->assertGuest();
+        User::factory()->create();
+
+        $response = $this->post('/api/v1/login', $data);
+
+        $response->assertStatus(ResponseAlias::HTTP_FORBIDDEN);
+        $this->assertGuest();
+    }
+
+    /**
+     * DataProvider
+     */
+    public function signupUsersArgsValidationDataProvider(): array
+    {
+        return [
+            'emailが空' => [
+                'data' => [
+                    'email' => 'some@example.com',
+                    'password' => 'invalidPassword',
+                ],
+            ],
+            'passwordが空' => [
+                'data' => [
+                    'email' => 'bar@example.com',
+                    'password' => '________',
+                ],
+            ],
+        ];
+    }
+}
