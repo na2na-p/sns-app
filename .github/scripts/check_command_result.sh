3dc04b2b512922909ebcc51b4d08167abbe72c8d
na2na-p <na2na@na2na.dev>
2023-01-22 19:14:28 +0900

    Fix

--- .github/scripts/check_command_result.sh
+++ .github/scripts/check_command_result.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function succeeded () {
-  status=$(aws ssm get-command-invocation --command-id $1 --instance-id $targetId)
+  status=$(aws ssm get-command-invocation --command-id "$1" --instance-id "$2")
 	# "Status": の後の文字列を取得
-	status=$(echo $status | sed -e 's/^.*Status": "//' -e 's/".*$//')
-  if [ $status = "Success" ]; then
+	status=$(echo "$status" | sed -e 's/^.*Status": "//' -e 's/".*$//')
+  if [ "$status" = "Success" ]; then
     echo true
   else
     echo false
@@ -13,21 +13,21 @@ function succeeded () {
 
 function getTargets () {
 	response=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=na2na-sns-app-backend" "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId")
-	targetId=$(echo $response | jq '.[][]')
-	targetId=$(echo $targetId | sed -e 's/^"//' -e 's/"$//')
+	targetId=$(echo "$response" | jq '.[][]')
+	targetId=$(echo "$targetId" | sed -e 's/^"//' -e 's/"$//')
 	echo $targetId
 }
 
 commandId=$1
-commandId=$(echo $commandId | sed -e 's/^"//' -e 's/"$//')
+commandId=$(echo "$commandId" | sed -e 's/^"//' -e 's/"$//')
 targetId=$(getTargets)
 
 i=1
 
 while [ $i -le 10 ];
 do
-  succeeded=$(succeeded $commandId $targetId)
-  if [ $succeeded ]; then
+  succeeded=$(succeeded "$commandId" "$targetId")
+  if [ "$succeeded" ]; then
     exit 0
   fi
   i=$($i + 1 )
