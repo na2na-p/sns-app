589709627a1cf667da3b32f1748b6ed0730c42e0
na2na-p <na2na@na2na.dev>
2023-01-23 17:02:46 +0900

    指摘事項対応 - お気に入り系を一つの正常系テストに統合 - 受け取ったメッセージ配列の順番を検証するように

--- packages/backend/tests/Feature/Http/Controllers/ListMessageMessagesControllerTest.php
+++ packages/backend/tests/Feature/Http/Controllers/ListMessageMessagesControllerTest.php
@@ -25,6 +25,21 @@ class ListMessageMessagesControllerTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
 
+        $messages = Message::with('user')->orderBy('created_at', 'desc')->paginate(2);
+
+        $messageIdsOderByIdDesc = Message::orderBy('id', 'desc')->pluck('id')->toArray();
+
+        $favoritter = User::factory()->create();
+        Favorite::factory()->create([
+            'user_id' => $favoritter->id,
+            'message_id' => $messages[0]->id,
+        ]);
+
+        Favorite::factory()->create([
+            'user_id' => $user->id,
+            'message_id' => $messages[1]->id,
+        ]);
+
         $response = $this->get('/api/v1/messages');
         $response->assertStatus(ResponseAlias::HTTP_OK);
 
@@ -40,6 +55,36 @@ class ListMessageMessagesControllerTest extends TestCase
                 'isFavorite',
             ],
         ]);
+
+        $response->assertJsonFragment([
+            'id' => $messages[0]->id,
+            'body' => $messages[0]->body,
+            'created_by' => $messages[0]->user->name,
+            'created_at' => $messages[0]->created_at,
+            'favoritesCount' => 1,
+            'isFavorite' => false,
+        ]);
+
+        $response->assertJsonFragment([
+            'id' => $messages[1]->id,
+            'body' => $messages[1]->body,
+            'created_by' => $messages[1]->user->name,
+            'created_at' => $messages[1]->created_at,
+            'favoritesCount' => 1,
+            'isFavorite' => true,
+        ]);
+
+        $receivedIdsArray = [];
+        foreach ($response->json() as $message) {
+            $receivedIdsArray[] = $message['id'];
+        }
+
+        $expectedIdsArray = [];
+        foreach ($messageIdsOderByIdDesc as $messageId) {
+            $expectedIdsArray[] = $messageId;
+        }
+
+        $this->assertEquals($receivedIdsArray, $expectedIdsArray);
     }
 
     /**
@@ -57,6 +102,12 @@ class ListMessageMessagesControllerTest extends TestCase
         $message = Message::orderBy('id', 'desc')->limit(2)->get();
         $queryLastMessageId = $message->first()->id;
         $responseHeadMessageId = $message->last()->id;
+
+        $expectedResponseMessages = Message::with('user')
+            ->where('id', '<', $queryLastMessageId)
+            ->orderBy('id', 'desc')
+            ->limit(9)
+            ->get();
 
         $response = $this->get('/api/v1/messages?lastMessageId='.$queryLastMessageId);
         $response->assertStatus(ResponseAlias::HTTP_OK);
@@ -77,6 +128,18 @@ class ListMessageMessagesControllerTest extends TestCase
         $response->assertJsonFragment([
             'id' => $responseHeadMessageId,
         ]);
+
+        $receivedIdsArray = [];
+        foreach ($response->json() as $message) {
+            $receivedIdsArray[] = $message['id'];
+        }
+
+        $expectedIdsArray = [];
+        foreach ($expectedResponseMessages as $message) {
+            $expectedIdsArray[] = $message->id;
+        }
+
+        $this->assertEquals($receivedIdsArray, $expectedIdsArray);
     }
 
     /**
@@ -92,6 +155,9 @@ class ListMessageMessagesControllerTest extends TestCase
         $this->actingAs($user);
 
         $perPage = 5;
+
+        $expectedResponseMessages = Message::with('user')->orderBy('id', 'desc')->paginate($perPage);
+
         $response = $this->get('/api/v1/messages?perPage='.$perPage);
         $response->assertStatus(ResponseAlias::HTTP_OK);
 
@@ -107,6 +173,18 @@ class ListMessageMessagesControllerTest extends TestCase
                 'isFavorite',
             ],
         ]);
+
+        $receivedIdsArray = [];
+        foreach ($response->json() as $message) {
+            $receivedIdsArray[] = $message['id'];
+        }
+
+        $expectedIdsArray = [];
+        foreach ($expectedResponseMessages as $message) {
+            $expectedIdsArray[] = $message->id;
+        }
+
+        $this->assertEquals($receivedIdsArray, $expectedIdsArray);
     }
 
     /**
@@ -116,20 +194,26 @@ class ListMessageMessagesControllerTest extends TestCase
      */
     public function testListMessageWithLastMessageIdAndPerPage(): void
     {
-        Message::factory()->count(10)->create();
+        Message::factory()->count(5)->create();
 
         $user = User::factory()->create();
         $this->actingAs($user);
 
-        $perPage = 5;
+        $perPage = 3;
         $message = Message::orderBy('id', 'desc')->limit(2)->get();
         $queryLastMessageId = $message->first()->id;
         $responseHeadMessageId = $message->last()->id;
 
+        $expectedResponseMessages = Message::with('user')
+            ->where('id', '<', $queryLastMessageId)
+            ->orderBy('id', 'desc')
+            ->limit($perPage)
+            ->get();
+
         $response = $this->get('/api/v1/messages?lastMessageId='.$queryLastMessageId.'&perPage='.$perPage);
         $response->assertStatus(ResponseAlias::HTTP_OK);
 
-        $response->assertJsonCount(5);
+        $response->assertJsonCount(3);
 
         $response->assertJsonStructure([
             '*' => [
@@ -145,87 +229,17 @@ class ListMessageMessagesControllerTest extends TestCase
         $response->assertJsonFragment([
             'id' => $responseHeadMessageId,
         ]);
-    }
 
-    /**
-     * 自分のお気に入り済みのメッセージが含まれる場合にただしくレスポンスできるか
-     *
-     * @return void
-     */
-    public function testListMessageWithAlreadyFavorited(): void
-    {
-        Message::factory()->count(10)->create();
+        $receivedIdsArray = [];
+        foreach ($response->json() as $message) {
+            $receivedIdsArray[] = $message['id'];
+        }
 
-        $user = User::factory()->create();
-        $this->actingAs($user);
+        $expectedIdsArray = [];
+        foreach ($expectedResponseMessages as $message) {
+            $expectedIdsArray[] = $message->id;
+        }
 
-        $message = Message::first();
-        Favorite::factory()->create([
-            'user_id' => $user->id,
-            'message_id' => $message->id,
-        ]);
-
-        $response = $this->get('/api/v1/messages');
-        $response->assertStatus(ResponseAlias::HTTP_OK);
-
-        $response->assertJsonCount(10);
-
-        $response->assertJsonStructure([
-            '*' => [
-                'id',
-                'body',
-                'created_by',
-                'created_at',
-                'favoritesCount',
-                'isFavorite',
-            ],
-        ]);
-
-        $response->assertJsonFragment([
-            'id' => $message->id,
-            'isFavorite' => true,
-        ]);
-    }
-
-    /**
-     * 他人のお気に入り済みのメッセージが含まれる場合にただしくレスポンスできるか
-     *
-     * @return void
-     */
-    public function testListMessageWithAlreadyFavoritedByOthers(): void
-    {
-        Message::factory()->count(10)->create();
-
-        $user = User::factory()->create();
-        $this->actingAs($user);
-
-        $favoritter = User::factory()->create();
-
-        $message = Message::first();
-        Favorite::factory()->create([
-            'user_id' => $favoritter->id,
-            'message_id' => $message->id,
-        ]);
-
-        $response = $this->get('/api/v1/messages');
-        $response->assertStatus(ResponseAlias::HTTP_OK);
-
-        $response->assertJsonCount(10);
-
-        $response->assertJsonStructure([
-            '*' => [
-                'id',
-                'body',
-                'created_by',
-                'created_at',
-                'favoritesCount',
-                'isFavorite',
-            ],
-        ]);
-
-        $response->assertJsonFragment([
-            'id' => $message->id,
-            'isFavorite' => false,
-        ]);
+        $this->assertEquals($receivedIdsArray, $expectedIdsArray);
     }
 }
