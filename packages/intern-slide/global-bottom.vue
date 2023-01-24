ee4ca975cd2d54060e5b1776a90f62dcfb8d5eb5
A.Yamamoto <49822810+na2na-p@users.noreply.github.com>
2023-01-24 12:57:17 +0900

    サーバサイドインターン/発表資料 (#127)
    * Add dependencies
    
    * Update dev command
    
    * Remove unused
    
    * Add Theme
    
    * Add 環境周りまで
    
    * Update Backend記事まで
    
    * フロントエンド構築まで
    
    * 一旦最後まで
    
    * Add Slide deploy
    
    * Update Actions Name
    
    * Fix typo
    
    * Add dependencies
    
    * Update .gitattributes
    
    * Update slides.md
    
    * Add exported pdf
    
    * Add command
    
    * Add comment追記
    
    * Update
    - 表紙追記
    - 目次を見やすく
    
    * バージョン情報追記
    
    * Update
    - OpenAPIスキーマ生成ライブラリに言及
    - 本番カンペ追記
    
    * Add page number
    
    * Update
    要件定義について追加
    
    * Add demo image
    
    * Update exported pdf
    
    * Fix 日付
    
    * Update comments
    
    * Update pdf

---
+++ packages/intern-slide/global-bottom.vue
@@ -0,0 +1,9 @@
+<!-- an example footer for pages -->
+<template>
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 right-0 p-2"
+  >
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+</template>
